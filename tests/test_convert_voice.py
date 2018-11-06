1d32a9518f56fca1ad1ab8e0e57117f694d3fc03
Iselix <iselix.x17351@gmail.com>
2018-11-06 22:11:27 +0900

    Check voice conversion result in test.

--- tests/test_convert_voice.py
+++ tests/test_convert_voice.py
@@ -3,13 +3,30 @@ import subprocess
 
 import pytest
 
-from tests import dataset
+import kwiiyatta
+
+from tests import dataset, feature
+from tests.plugin import assert_any
 
 
 pytestmark = pytest.mark.slow
 
 
-def test_voice_conversion(tmpdir):
+def make_expected_feature(wavpath, fullset=False):
+    src = feature.get_analyzer(
+        dataset.get_wav_path(dataset.CLB_DIR/wavpath, fullset))
+    tgt = feature.get_analyzer(
+        dataset.get_wav_path(dataset.SLT_DIR/wavpath, fullset))
+    tgt_aligned = kwiiyatta.align(tgt, src)
+
+    expected = kwiiyatta.feature(src)
+    expected.spectrum_envelope = tgt_aligned.spectrum_envelope
+    feature.override_spectrum_power(expected, src)
+
+    return expected
+
+
+def test_voice_conversion(tmpdir, check):
     result_root = pathlib.Path(tmpdir)
     subprocess.run(
         [
@@ -22,7 +39,26 @@ def test_voice_conversion(tmpdir):
     assert (result_root/'arctic_a0009.diff.wav').is_file()
     assert (result_root/'arctic_a0009.synth.wav').is_file()
 
+    expected = make_expected_feature('arctic_a0009.wav')
 
+    act_diff = kwiiyatta.analyze_wav(result_root/'arctic_a0009.diff.wav')
+    f0_diff, spec_diff, ape_diff, mcep_diff = \
+        feature.calc_feature_diffs(expected, act_diff)
+    check.round_equal(0.12, f0_diff)
+    check.round_equal(0.69, spec_diff)
+    check.round_equal(0.072, ape_diff)
+    check.round_equal(0.26, mcep_diff)
+
+    act_synth = kwiiyatta.analyze_wav(result_root/'arctic_a0009.synth.wav')
+    f0_diff, spec_diff, ape_diff, mcep_diff = \
+        feature.calc_feature_diffs(expected, act_synth)
+    check.round_equal(0.11, f0_diff)
+    check.round_equal(0.77, spec_diff)
+    check.round_equal(0.080, ape_diff)
+    check.round_equal(0.25, mcep_diff)
+
+
+@pytest.mark.assert_any
 def test_voice_conversion_fullset(tmpdir):
     result_root = pathlib.Path(tmpdir)
     subprocess.run(
@@ -40,5 +76,26 @@ def test_voice_conversion_fullset(tmpdir):
         result_path = (result_root/result).with_suffix('.diff.wav')
         assert result_path.is_file()
 
+        expected = make_expected_feature(result+'.wav', fullset=True)
+
+        actual = kwiiyatta.analyze_wav(result_path)
+        f0_diff, spec_diff, ape_diff, mcep_diff = \
+            feature.calc_feature_diffs(expected, actual)
+        assert_any.between(0.028, f0_diff, 0.054)
+        assert_any.between(0.35, spec_diff, 0.42)
+        assert_any.between(0.026, ape_diff, 0.044)
+        assert_any.between(0.17, mcep_diff, 0.18)
+
+    for result in results:
         result_path = (result_root/result).with_suffix('.synth.wav')
         assert result_path.is_file()
+
+        expected = make_expected_feature(result+'.wav', fullset=True)
+
+        actual = kwiiyatta.analyze_wav(result_path)
+        f0_diff, spec_diff, ape_diff, mcep_diff = \
+            feature.calc_feature_diffs(expected, actual)
+        assert_any.between(0.048, f0_diff, 0.11)
+        assert_any.between(0.36, spec_diff, 0.47)
+        assert_any.between(0.068, ape_diff, 0.11)
+        assert_any.between(0.17, mcep_diff, 0.19)
