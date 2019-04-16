9bb6379f075e14b404a7c0c9e059642badff30a3
Iselix <iselix.x17351@gmail.com>
2019-04-17 00:26:53 +0900

    Add voice conversion test with fullset data.

--- tests/test_convert_voice.py
+++ tests/test_convert_voice.py
@@ -21,3 +21,24 @@ def test_voice_conversion(tmpdir):
 
     assert (result_root/'arctic_a0009.diff.wav').is_file()
     assert (result_root/'arctic_a0009.synth.wav').is_file()
+
+
+def test_voice_conversion_fullset(tmpdir):
+    result_root = pathlib.Path(tmpdir)
+    subprocess.run(
+        [
+            'python', 'convert_voice.py',
+            '--data-root', str(dataset.get_dataset_path(dataset.DATASET_ROOT,
+                                                        fullset=True)),
+            '--result-dir', str(result_root),
+            '--gmm-seed', '0',
+        ], check=True)
+
+    results = ['arctic_a0036', 'arctic_a0041', 'arctic_a0082']
+
+    for result in results:
+        result_path = (result_root/result).with_suffix('.diff.wav')
+        assert result_path.is_file()
+
+        result_path = (result_root/result).with_suffix('.synth.wav')
+        assert result_path.is_file()
