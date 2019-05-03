df8ce68b40b7fec28919ddfe4cdd5ea828803d12
Iselix <iselix.x17351@gmail.com>
2019-05-03 19:22:07 +0900

    Add qu_uic setup command.

--- setup.py
+++ setup.py
@@ -1,4 +1,38 @@
+import distutils.log
+import pathlib
+import subprocess
+
 import setuptools
+import setuptools.command.build_py
+
+
+class QtUicCommand(setuptools.Command):
+    description = 'run pyside2-uic command to convert ui to py'
+    user_options = []
+    ui_paths = [
+        pathlib.Path('kwiiyatta/view/qt/ui')
+    ]
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        command = 'pyside2-uic'
+        for p in self.ui_paths:
+            for f in p.glob('*.ui'):
+                commands = [
+                    command,
+                    str(f),
+                    '-o', str(f.with_suffix('.py'))
+                ]
+                self.announce(
+                    'Running command: %s' % str(commands),
+                    level=distutils.log.INFO)
+                subprocess.check_call(commands)
+
 
 setuptools.setup(
     name="kwiiyatta",
@@ -22,5 +56,8 @@ setuptools.setup(
             'kwiiyatta=kwiiyatta.convert_voice:main',
             'kwiieiya=kwiiyatta.resynthesize_voice:main',
         ],
+    },
+    cmdclass={
+        'qt_uic': QtUicCommand,
     },
 )
